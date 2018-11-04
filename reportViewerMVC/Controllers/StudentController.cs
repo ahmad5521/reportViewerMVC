d7cbdc9ae507ff88593940f1cdd63799d5a0fc68
ahmad asiri <a.wv@live.com>
2018-11-04 13:03:44 +0300

    commit

---
+++ reportViewerMVC/Controllers/StudentController.cs
@@ -0,0 +1,47 @@
+﻿using Microsoft.Reporting.WebForms;
+using reportViewerMVC.Reports;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.UI.WebControls;
+
+namespace reportViewerMVC.Controllers
+{
+    public class StudentController : Controller
+    {
+        // GET: Student
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        MyDataSet ds = new MyDataSet();
+        public ActionResult ReportEmployee()
+        {
+            ReportViewer reportViewer = new ReportViewer();
+            reportViewer.ProcessingMode = ProcessingMode.Local;
+            reportViewer.SizeToReportContent = true;
+            reportViewer.Width = Unit.Percentage(900);
+            reportViewer.Height = Unit.Percentage(900);
+
+            var connectionString = ConfigurationManager.ConnectionStrings["reportViewerMVC.Properties.Settings.TestDB_OmarConnectionString"].ConnectionString;
+
+
+            SqlConnection conx = new SqlConnection(connectionString); SqlDataAdapter adp = new SqlDataAdapter("SELECT * FROM Student", conx);
+
+            adp.Fill(ds, ds.Student.TableName);
+
+            reportViewer.LocalReport.ReportPath = Request.MapPath(Request.ApplicationPath) + @"Reports\MyReport.rdlc";
+            reportViewer.LocalReport.DataSources.Add(new ReportDataSource("MyDataSet", ds.Tables[0]));
+
+
+            ViewBag.ReportViewer = reportViewer;
+
+            return View();
+        }
+    }
+}
